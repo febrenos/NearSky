1749e3369b38374c42cced9645a3dbc5f93ee2c5
febreno <felipesugisawa1@gmail.com>
2024-02-23 14:19:13 -0300

    second commit

--- src/components/cards/weather/List/style.js
+++ src/components/cards/weather/List/style.js
@@ -4,6 +4,7 @@ export const Cards = styled.div`
   display:grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap:20px;
+  justify-items: center;
   // place-items: center;
   // padding:  0 0 45px 0;
 
